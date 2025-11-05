af1fde8387757fafaa2df0c70f383bdb61311aed
sergejtoropov086-pixel <sergejtoropov086@gmail.com>
2025-11-05 23:09:46 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = 'fixa-v1';
-const urlsToCache = ['./', './index.html', './manifest.json', './icon-192x192.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
